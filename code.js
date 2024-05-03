1b66d059ad9166e1c93afb5e43516e56fc69f61f
bellamoss77 <mmf295@icloud.com>
2024-05-02 22:13:05 -0400

    start CSS, fix vector

--- code.js
+++ code.js
@@ -11,6 +11,10 @@ document.addEventListener('DOMContentLoaded', function() {
     pageTitle.className = 'gallery-title';
     pageTitle.textContent = 'Des Fleurs';
     headerContainer.appendChild(pageTitle);
+    if (!pageTitle) {
+        console.log('No page title found.')
+        return;
+    }
 
     const pageSubtitle = document.createElement('h3');
     pageSubtitle.className = 'gallery-subtitle';
@@ -18,11 +22,14 @@ document.addEventListener('DOMContentLoaded', function() {
     headerContainer.appendChild(pageSubtitle);
 
     const titleVector = document.createElement('img');
-    titleVector.setAttribute.src = 'images/flower-vector.png';
+    titleVector.src = 'https://github.com/bellamoss77/scroll-loop-gallery/blob/main/images/flower-vector.png?raw=true';
     titleVector.className = 'vector';
     headerContainer.appendChild(titleVector);
 
     const galleryWrapper = document.getElementById('galleryWrapper');
+    body.appendChild(galleryWrapper);
+
+    body.insertBefore(headerContainer, galleryWrapper);
 
     const thumbList = document.createElement('ul');
     thumbList.className = 'thumb-list';
@@ -52,23 +59,24 @@ document.addEventListener('DOMContentLoaded', function() {
     galleryBtnsContainer.appendChild(openLightboxBtn);
 
     const images = [
-        { src: 'images/fleurs_1.png', alt: 'flower' },
-        { src: 'images/fleurs_2.png', alt: 'flower' },
-        { src: 'images/fleurs_3.png', alt: 'flower' },
-        { src: 'images/fleurs_4.png', alt: 'flower' },
-        { src: 'images/fleurs_5.png', alt: 'flower' },
-        { src: 'images/fleurs_6.png', alt: 'flower' },
-        { src: 'images/fleurs_7.png', alt: 'flower' },
-        { src: 'images/fleurs_8.png', alt: 'flower' },
-        { src: 'images/fleurs_9.png', alt: 'flower' },
-        { src: 'images/fleurs_10.png', alt: 'flower' },
-        { src: 'images/fleurs_11.png', alt: 'flower' },
-        { src: 'images/fleurs_12.png', alt: 'flower' },
-        { src: 'images/fleurs_13.png', alt: 'flower' },
-        { src: 'images/sunset_1.png', alt: 'sunset' },
-        { src: 'images/sunset_2.png', alt: 'sunset' },
+        { src: 'https://github.com/bellamoss77/scroll-loop-gallery/blob/main/images/fleurs_1%20.png?raw=true', alt: 'flower' },
+        { src: 'https://github.com/bellamoss77/scroll-loop-gallery/blob/main/images/fleurs_10.png?raw=true', alt: 'flower' },
+        { src: 'https://github.com/bellamoss77/scroll-loop-gallery/blob/main/images/fleurs_11.png?raw=true', alt: 'flower' },
+        { src: 'https://github.com/bellamoss77/scroll-loop-gallery/blob/main/images/fleurs_12.png?raw=true', alt: 'flower' },
+        { src: 'https://github.com/bellamoss77/scroll-loop-gallery/blob/main/images/fleurs_13.png?raw=true', alt: 'flower' },
+        { src: 'https://github.com/bellamoss77/scroll-loop-gallery/blob/main/images/fleurs_2.png?raw=true', alt: 'flower' },
+        { src: 'https://github.com/bellamoss77/scroll-loop-gallery/blob/main/images/fleurs_3.png?raw=true', alt: 'flower' },
+        { src: 'https://github.com/bellamoss77/scroll-loop-gallery/blob/main/images/fleurs_4.png?raw=true', alt: 'flower' },
+        { src: 'https://github.com/bellamoss77/scroll-loop-gallery/blob/main/images/fleurs_5.png?raw=true', alt: 'flower' },
+        { src: 'https://github.com/bellamoss77/scroll-loop-gallery/blob/main/images/fleurs_6.png?raw=true', alt: 'flower' },
+        { src: 'https://github.com/bellamoss77/scroll-loop-gallery/blob/main/images/fleurs_7.png?raw=true', alt: 'flower' },
+        { src: 'https://github.com/bellamoss77/scroll-loop-gallery/blob/main/images/fleurs_8.png?raw=true', alt: 'flower' },
+        { src: 'https://github.com/bellamoss77/scroll-loop-gallery/blob/main/images/fleurs_9.png?raw=true', alt: 'flower' },
+        { src: 'https://github.com/bellamoss77/scroll-loop-gallery/blob/main/images/sunset_1.png?raw=true', alt: 'sunset' },
+        { src: 'https://github.com/bellamoss77/scroll-loop-gallery/blob/main/images/sunset_2.png?raw=true', alt: 'sunset' },
     ]
 
+    
     function createGallery() {
         images.forEach(image => {
             const img = document.createElement('img');
@@ -77,6 +85,7 @@ document.addEventListener('DOMContentLoaded', function() {
             galleryWrapper.appendChild(img);
         })
     }
+    createGallery();
 
     
 })
